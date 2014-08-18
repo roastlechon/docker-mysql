352813a26147f81c16e1444895639b1cbd9d811a
Noel Madali <amadali@gmail.com>
2014-08-18 00:02:57 -0400

    modified how comments were echoed out

--- my_init.d/99_mysql_setup.sh
+++ my_init.d/99_mysql_setup.sh
@@ -2,39 +2,40 @@
 
 if [ ! -d /var/lib/mysql/mysql ]; then
     touch ~/test1
-    echo 'Rebuilding mysql data dir'
+    echo '*** Rebuilding mysql data dir'
         
     chown -R mysql.mysql /var/lib/mysql
     mysql_install_db > /dev/null
 
     rm -rf /var/run/mysqld/*
 
-    echo 'Starting mysqld'
+    echo '*** Starting mysqld'
     # The sleep 1 is there to make sure that inotifywait starts up before the socket is created
     mysqld_safe &
 
-    echo 'Waiting for mysqld to come online'
+    echo '*** Waiting for mysqld to come online'
     while [ ! -x /var/run/mysqld/mysqld.sock ]; do
         sleep 1
     done
     
-    echo 'Setting root password to root'
+    echo '*** Setting root password to root'
     /usr/bin/mysqladmin -u root password 'root'
 
+    echo '*** Creating mysql user: ${MYSQL_USER}'
     mysql -uroot -proot -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '$MYSQL_PASS'"
     mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION"
 
+    echo '*** Bootstrapping database with scripts found in /root/setup'
     if [ -d /root/setup ]; then
-        echo 'Found /root/setup - scanning for SQL scripts'
         for sql in $(ls /root/setup/*.sql 2>/dev/null | sort); do
-            echo 'Found script - running:' $sql
+            echo '*** Running script:' $sql
             mysql -uroot -proot -e "\. $sql"
         done
     else
-        echo 'No setup directory with extra sql scripts to run'
+        echo '*** Directory /root/setup does not exist'
     fi
 
-    echo 'Shutting down mysqld'
+    echo '*** Shutting down mysqld'
     mysqladmin -uroot -proot shutdown
 
 fi
